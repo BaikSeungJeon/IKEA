ac9a02169e6aab74051948c19c22fd8771c046d5
baikseungjeon <tmdwjsofficial@naver.com>
2021-10-14 15:58:26 +0900

    1014

--- js/sales.js
+++ js/sales.js
@@ -1,9 +1,48 @@
 // chart js
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var ctx1 = document.getElementById('myChart1').getContext('2d'); // 첫 번째 일일 차트
+var ctx2 = document.getElementById('myChart2').getContext('2d'); // 두 번째 토탈 차트
+
+// 첫 번째 일일 차트
+var myChart = new Chart(ctx1, {
+    type: 'line', // 꺾은선 그래프
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+// 두 번째 토탈 차트
+var myChart = new Chart(ctx2, {
+    type: 'line', // line 꺾은선 그래프
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
         datasets: [{
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
